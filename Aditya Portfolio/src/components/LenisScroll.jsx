412fc1fd455de6fc3fc0d8c7dde01172b0f51933
ADITYA SINGH <adityaaasingh47@gmail.com>
2025-05-01 21:40:47 +0530

    Update LenisScroll.jsx

--- Aditya Portfolio/src/components/LenisScroll.jsx
+++ Aditya Portfolio/src/components/LenisScroll.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from 'lenis';
 
 function SmoothScroll({ children }) {
     const lenisRef = useRef(null);
